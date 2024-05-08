398e9c055c67643285c76b79037d6b5d6150d03d
Juan Diaz <juannavegante2010@gmail.com>
2024-05-07 21:47:58 -0500

    Se añade test para sub

--- src/01-sub/sub.js
+++ src/01-sub/sub.js
@@ -1,8 +1,10 @@
 function sub(num1,num2){
     if(num1 === undefined || num2 === undefined){
         return "Ah ocurrido un error, no se han recibido argumentos.";
+    }else if(typeof num1 != "number" || typeof num2 != "number"){
+        return "Ah ocurrido un error, no se han recibido argumentos.";
     }
-    return num1-num2;
+    return Math.abs(num1-num2);
 }
 
 module.exports = sub;
